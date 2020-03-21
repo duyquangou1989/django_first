52674ba8d8a4886b40b56dca304ef0b1ab84679e
duyquangou1989 <duyquangou1989@gmail.com>
2020-03-22 00:49:08 +0700

    Update detail polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,17 +8,27 @@ from .models import Question
 #def index(request):
 #	return HttpResponse("Hello World")
 
+'''
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	'''
-	output = ', '.join([q.question_text for q in latest_question_list])
-	return HttpResponse(output)
-	'''
+	#test = Question.objects.all()
+	#output = ', '.join([q.question_text for q in latest_question_list])
+	#return HttpResponse(output)
 	template = loader.get_template('polls/index.html')
 	context = {
 		'latest_question_list': latest_question_list,
 	}
 	return HttpResponse(template.render(context, request))
+	#return HttpResponse(test)
+'''
+
+#Shortcut : Render
+def index(request):
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return render(request,'polls/index.html',context)
 
 def detail(request, question_id):
 	return HttpResponse("You are looking at question %s " % question_id)
